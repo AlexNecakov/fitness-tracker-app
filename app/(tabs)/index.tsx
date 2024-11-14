1055459f2ee2e12cc6f63c3ec54be4cf054d770b
Alex Necakov <alexnecakov@gmail.com>
2024-11-14 08:34:11 -0500

    turned exercise entries into component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,40 +1,18 @@
-import { useState } from 'react';
 import { Text, View, StyleSheet, } from "react-native";
 
-import IconButton from "@/components/IconButton";
+import ExerciseRow from '@/components/ExerciseRow';
 
 
 export default function Index() {
-    const [numPullUps, setNumPullUps] = useState(0);
-
-    const onPlus = () => {
-        setNumPullUps(numPullUps + 1);
-    };
-    const onMinus = () => {
-        setNumPullUps(Math.max(0, numPullUps - 1));
-    };
-
-
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Set # 2/3</Text>
-            <Text style={styles.text}>Last  Current</Text>
-            <View style={styles.optionsContainer}>
-                <View style={styles.optionsRow}>
-                    <Text style={styles.text}>Pull-Ups  5 </Text>
-                    <IconButton icon="minus" onPress={onMinus} ></IconButton>
-                    <Text style={styles.text}>{numPullUps}</Text>
-                    <IconButton icon="plus" onPress={onPlus} ></IconButton>
-                </View>
-                <View style={styles.optionsRow}>
-                    <Text style={styles.text}>S. Rows   5 -5+</Text>
-                </View>
-                <View style={styles.optionsRow}>
-                    <Text style={styles.text}>Dips      5 -5+</Text>
-                </View>
-                <View style={styles.optionsRow}>
-                    <Text style={styles.text}>Push-Ups  5 -5+</Text>
-                </View>
+            <View style={styles.entriesContainer}>
+                <Text style={styles.text}>Set # 2/3</Text>
+                <Text style={styles.text}>Last  Current</Text>
+                <ExerciseRow label="Pull-Ups"></ExerciseRow>
+                <ExerciseRow label="S. Rows"></ExerciseRow>
+                <ExerciseRow label="Dips"></ExerciseRow>
+                <ExerciseRow label="Push-Ups"></ExerciseRow>
             </View>
         </View >
     );
@@ -51,29 +29,17 @@ const styles = StyleSheet.create({
         fontSize: 40,
         color: '#ffff',
     },
-    button: {
-        fontSize: 20,
-        textDecorationLine: 'underline',
-        color: "#fff",
-    },
-    imageContainer: {
-        flex: 1,
-    },
-    image: {
-        width: 320,
-        height: 440,
-        borderRadius: 18,
-    },
-    footerContainer: {
-        flex: 1 / 3,
-        alignItems: 'center',
-    },
-    optionsContainer: {
+    entriesContainer: {
         position: 'absolute',
         bottom: 80,
     },
-    optionsRow: {
+    entriesRow: {
         alignItems: 'center',
         flexDirection: 'row',
+    },
+    entriesCell: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        padding: 20,
     }
 });
---
+++ components/ExerciseRow.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { Text, View, StyleSheet, } from "react-native";
+
+import IconButton from "@/components/IconButton";
+
+type Props = {
+    label: string;
+};
+
+export default function ExerciseRow({ label }: Props) {
+    const [numReps, setNumReps] = useState(0);
+
+    const onPlus = () => {
+        setNumReps(numReps + 1);
+    };
+    const onMinus = () => {
+        setNumReps(Math.max(0, numReps - 1));
+    };
+
+
+    return (
+        <View style={styles.entriesRow}>
+            <View style={styles.entriesCell}>
+                <Text style={styles.text}>{label}</Text>
+            </View>
+            <View style={styles.entriesCell}>
+                <Text style={styles.text}>5</Text>
+            </View>
+            <View style={styles.entriesCell}>
+                <IconButton icon="minus" onPress={onMinus} ></IconButton>
+                <Text style={styles.text}>{numReps}</Text>
+                <IconButton icon="plus" onPress={onPlus} ></IconButton>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 40,
+        color: '#ffff',
+    },
+    entriesRow: {
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    entriesCell: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        padding: 20,
+    }
+});
