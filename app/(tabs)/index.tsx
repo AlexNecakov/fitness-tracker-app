eec808119f305dfde21401423fc195c508d8457d
Alex Necakov <alexnecakov@gmail.com>
2024-11-15 08:59:31 -0500

    added full schema and numset

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,6 +9,8 @@ import Button from "@/components/Button";
 export default function Index() {
 
     const [db, setDb] = useState<SQLite.SQLiteDatabase | undefined>(undefined);
+    const [workoutId, setWorkoutId] = useState<number>(0);
+    const [numSet, setNumSet] = useState<number>(1);
     const [numPullUps, setNumPullUps] = useState<number>(5);
     const [numRows, setNumRows] = useState<number>(5);
     const [numDips, setNumDips] = useState<number>(5);
@@ -19,10 +21,10 @@ export default function Index() {
             try { // Replace with your async function 
                 const datab = await SQLite.openDatabaseAsync('databaseName');
                 setDb(datab);
-                if (db) {
-                    await db.execAsync(`
+                if (datab) {
+                    await datab.execAsync(`
                     PRAGMA journal_mode = WAL;
-                    CREATE TABLE IF NOT EXISTS lifts (id INTEGER PRIMARY KEY NOT NULL, lift TEXT NOT NULL, numReps INTEGER);
+                    CREATE TABLE IF NOT EXISTS lifts (id INTEGER PRIMARY KEY NOT NULL, workoutId INTEGER NOT NULL, dateTime INTEGER NOT NULL, numSet INTEGER NOT NULL, lift TEXT NOT NULL, numReps INTEGER);
                 `);
                 }
             } catch (error) { console.error(error); }
@@ -40,14 +42,15 @@ export default function Index() {
 
     const onSubmitSet = async () => {
         try {
-            await db.runAsync(`INSERT INTO lifts (lift, numReps) VALUES ('pullups', $numPullUps)`, { $numPullUps: numPullUps });
-            await db.runAsync(`INSERT INTO lifts (lift, numReps) VALUES ('rows', $numRows)`, { $numRows: numRows });
-            await db.runAsync(`INSERT INTO lifts (lift, numReps) VALUES ('dips', $numDips)`, { $numDips: numDips });
-            await db.runAsync(`INSERT INTO lifts (lift, numReps) VALUES ('pushups', $numPushUps)`, { $numPushUps: numPushUps });
+            await db.runAsync(`INSERT INTO lifts (workoutId, dateTime, numSet, lift, numReps) VALUES ($workoutId, datetime(), $numSet, 'pullups', $numPullUps)`, { $workoutId: workoutId, $numSet: numSet, $numPullUps: numPullUps });
+            await db.runAsync(`INSERT INTO lifts (workoutId, dateTime, numSet, lift, numReps) VALUES ($workoutId, datetime(), $numSet, 'rows', $numRows)`, { $workoutId: workoutId, $numSet: numSet, $numRows: numRows });
+            await db.runAsync(`INSERT INTO lifts (workoutId, dateTime, numSet, lift, numReps) VALUES ($workoutId, datetime(), $numSet, 'dips', $numDips)`, { $workoutId: workoutId, $numSet: numSet, $numDips: numDips });
+            await db.runAsync(`INSERT INTO lifts (workoutId, dateTime, numSet, lift, numReps) VALUES ($workoutId, datetime(), $numSet, 'pushups', $numPushUps)`, { $workoutId: workoutId, $numSet: numSet, $numPushUps: numPushUps });
             const allRows = await db.getAllAsync('SELECT * FROM lifts');
             for (const row of allRows) {
-                console.log(row.id, row.lift, row.numReps);
+                console.log(row.id, row.workoutId, row.dateTime, row.numSet, row.lift, row.numReps);
             }
+            setNumSet(numSet + 1);
         }
         catch (error) {
             console.error('Database error:', error);
@@ -58,7 +61,7 @@ export default function Index() {
     return (
         <View style={styles.container}>
             <View style={styles.entriesContainer}>
-                <Text style={styles.text}>Set # 2/3</Text>
+                <Text style={styles.text}>Set # {numSet}/3</Text>
                 <Text style={styles.text}>Last  Current</Text>
                 <ExerciseRow label="Pull-Ups" numReps={numPullUps} setNumReps={setNumPullUps}></ExerciseRow>
                 <ExerciseRow label="S. Rows" numReps={numRows} setNumReps={setNumRows}></ExerciseRow>
